e23960d566106dc4bbf8e78dc8feebcd74385720
Oleg Blashchuk <40044603+contras2k@users.noreply.github.com>
2018-06-11 00:23:41 +0300

    Task 3 ver. 1

---
+++ task3.rb
@@ -0,0 +1,53 @@
+# Task 3
+
+if ARGV.length < 2
+  exit(1)
+  end
+
+t1 = ARGV[0].to_i
+t2 = ARGV[1].to_i
+
+if t1 < 1 or t1 > 100000 or t2 < 1 or t2 > 100000
+  exit(1)
+  end
+
+t = t1 + t2
+
+result=""
+
+hours = t / 3600
+if hours > 0
+  h0 = hours % 10
+  suffix = " "
+  if (5..9) === h0 or h0 == 0 or (11..14) === hours
+    suffix = "ов "
+  elsif (2..4) === h0
+    suffix = "а "
+    end
+  result = hours.to_s + " час" + suffix
+  end
+
+secs = t % 3600
+mins = secs / 60
+if hours > 0 or mins > 0
+  m0 = mins % 10
+  suffix = "а "
+  if (5..9) === m0 or m0 == 0 or (11..14) === mins
+    suffix = " "
+  elsif (2..4) === m0
+    suffix = "ы "
+    end
+  result = result + mins.to_s + " минут" + suffix
+  end
+
+secs %= 60
+s0 = secs % 10
+suffix = "а"
+if (5..9) === s0 or s0 == 0 or (11..14) === secs
+  suffix = ""
+elsif (2..4) === s0
+  suffix = "ы"
+  end
+result = result + secs.to_s + " секунд" + suffix
+
+puts result
