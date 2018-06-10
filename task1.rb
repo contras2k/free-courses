43fd00b8640f70ce79a2bb72605a3e3a56456798
Oleg Blashchuk <40044603+contras2k@users.noreply.github.com>
2018-06-10 22:49:25 +0300

    Task 1 complete

--- task1.rb
+++ task1.rb
@@ -1,3 +1,25 @@
 # Task 1
 
-# Put your code here
+if ARGV.length < 3
+  exit(1)
+  end
+
+a = ARGV[1].to_i
+b = ARGV[2].to_i
+
+if a<2 or a>36 or b<2 or b>36
+  exit(1)
+  end
+
+if a == b
+  puts ARGV[0]
+  exit(0)
+  end
+
+i = ARGV[0].to_i(base=a)
+
+if i < 0 or i > 10000
+  exit(1)
+  end
+
+puts i.to_s(base=b)
