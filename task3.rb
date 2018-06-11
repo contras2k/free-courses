3e6cc83d4de3c5b4be2f35f9255ae8b2936ddc98
Oleg Blashchuk <40044603+contras2k@users.noreply.github.com>
2018-06-11 10:49:25 +0300

    Task 3: correct possible trailing space

--- task3.rb
+++ task3.rb
@@ -22,27 +22,33 @@ secs %= 60
 
 if hours > 0
   h0 = hours % 10
-  suffix = " "
+  suffix = ""
   if (5..9) === h0 or h0 == 0 or (11..14) === hours
-    suffix = "ов "
+    suffix = "ов"
   elsif (2..4) === h0
-    suffix = "а "
+    suffix = "а"
     end
   result = hours.to_s + " час" + suffix
   end
 
 if mins > 0
-  m0 = mins % 10
-  suffix = "а "
-  if (5..9) === m0 or m0 == 0 or (11..14) === mins
-    suffix = " "
-  elsif (2..4) === m0
-    suffix = "ы "
+  if result != ""
+    result += " "
     end
-  result = result + mins.to_s + " минут" + suffix
+  m0 = mins % 10
+  suffix = "а"
+  if (5..9) === m0 or m0 == 0 or (11..14) === mins
+    suffix = ""
+  elsif (2..4) === m0
+    suffix = "ы"
+    end
+  result += mins.to_s + " минут" + suffix
   end
 
 if secs > 0
+  if result != ""
+    result += " "
+    end
   s0 = secs % 10
   suffix = "а"
   if (5..9) === s0 or s0 == 0 or (11..14) === secs
@@ -50,7 +56,7 @@ if secs > 0
   elsif (2..4) === s0
     suffix = "ы"
     end
-  result = result + secs.to_s + " секунд" + suffix
+  result += secs.to_s + " секунд" + suffix
   end
 
 puts result
